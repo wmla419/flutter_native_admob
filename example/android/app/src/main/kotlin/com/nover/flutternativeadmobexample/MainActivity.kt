1b1852920cb19f1190be720cfc22131523aaf395
wmla419 <43129451+wmla419@users.noreply.github.com>
2024-04-23 23:11:38 +0800

    Update MainActivity.kt

--- example/android/app/src/main/kotlin/com/nover/flutternativeadmobexample/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/nover/flutternativeadmobexample/MainActivity.kt
@@ -1,20 +1,5 @@
 package com.nover.flutternativeadmobexample
 
-import android.os.Bundle
-import com.google.android.gms.ads.MobileAds
-import com.google.android.gms.ads.RequestConfiguration
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-
-    companion object {
-        const val TEST_DEVICE_ID = ""
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        val configuration = RequestConfiguration.Builder()
-            .setTestDeviceIds(listOf(TEST_DEVICE_ID)).build()
-        MobileAds.setRequestConfiguration(configuration)
-    }
-}
+class MainActivity : FlutterActivity() {}
